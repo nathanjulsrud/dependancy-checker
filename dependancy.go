93dca33e754a596a53cfd21065e440b8e7934178
Nathan Julsrud <nathan.julsrud@mastery.net>
2021-07-02 10:01:40 -0500

    more code

--- dependancy.go
+++ dependancy.go
@@ -4,62 +4,67 @@ import (
 	"fmt"
 )
 
-func buildPath(services map[string]string, checked []string, checkAgainst string) []string {
-	if services[checkAgainst] == "" {
+func buildPath(services map[string][]string, checked [][]string, currentThread []string, checkAgainst string) [][]string {
+	if len(services[checkAgainst]) == 0 {
 		//if nothing more to follow append item and end process
-		checked = append(checked, checkAgainst)
+		fmt.Println("zero")
+		checked = append(checked, currentThread)
 		return checked
-	} else if services[checkAgainst] == checkAgainst {
+	} else if contains(services[checkAgainst], checkAgainst) == true {
 		//if self referencing append item and end process
-		checked = append(checked, services[checkAgainst])
-		checked = append(checked, services[checkAgainst])
+		currentThread = append(currentThread, checkAgainst)
+		checked = append(checked, currentThread)
+		fmt.Println("self reference")
 		return checked
 	} else {
-		checked = append(checked, checkAgainst)
-		for checkService := range checked {
-			var serviceCount int = 0
-			for checkAgainst := range checked {
-				if checked[checkService] == checked[checkAgainst] {
-					serviceCount++
-				}
-			}
-			//if count of any stop is greater than one this is a loop
-			if serviceCount > 1 {
+		fmt.Println("else!")
+		//map level compare
+		for checkNext := range services[checkAgainst] {
+			currentThread = append(currentThread, services[checkAgainst][checkNext])
+			//figure out if duplicate item in currentThread
+			if isLoop(currentThread) == true {
+				checked = append(checked, currentThread)
 				return checked
 			}
+			//checked = buildPath(services, checked, currentThread, services[checkAgainst])
 		}
-		checked = buildPath(services, checked, services[checkAgainst])
 	}
 	return checked
 }
 
-func findDependancyLoop(services map[string][]string) bool {
-	//resultMap := make(map[string]bool)
-	for compareService := range services {
-		fmt.Println(compareService)
-		subDependancy := services[compareService]
-		fmt.Println("sub", subDependancy)
-		for compare := range subDependancy {
-			fmt.Println(subDependancy[compare])
+func contains(stringSlice []string, stringFind string) bool {
+	for value := range stringSlice {
+		if stringSlice[value] == stringFind {
+			return true
 		}
-		/*for dependancy := range services[compareService] {
-			fmt.Println("service", services[compareService])
-			fmt.Println("depend", dependancy)
-		}*/
 	}
+	return false
+}
 
+func isLoop(stringSlice []string) bool {
+	for checkService := range stringSlice {
+		var serviceCount int = 0
+		for checkAgainst := range stringSlice {
+			if stringSlice[checkService] == stringSlice[checkAgainst] {
+				serviceCount++
+				//if count of any stop is greater than one this is a loop
+				if serviceCount > 1 {
+					return true
+				}
+			}
+		}
+	}
 	return false
 }
 
 func main() {
 	dependMap := map[string][]string{
-		"a": {},
-		"b": {"a"},
+		"a": {"a"},
+		"b": {""},
 		"c": {"b", "a"},
 	}
 
-	result := findDependancyLoop(dependMap)
-	fmt.Println(result)
-	//fmt.Println("result", buildPath(dependMap, make([]string, 0), service))
+	tempSlice := []string{"a"}
+	fmt.Println("result", buildPath(dependMap, make([][]string, 0), tempSlice, "a"))
 
 }
